105428e11a33039fa306cbf875d56f97ba59ab23
chizakasipascal <pascalkasichiza@gmail.com>
2022-03-16 22:13:56 +0200

    Chargement de la couleur blac  et organisation de l'interface homscreen

--- lib/src/utils/colors.dart
+++ lib/src/utils/colors.dart
@@ -2,7 +2,8 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 const whiteColor = Color(0xFFFFFFFF);
-const blackColor = Color(0xFF393938);
+const blackColor = Color.fromARGB(255, 0, 0, 0);
+//const blackColor = Color(0xFF393938);
 const background = Color(0xFFffffff);
 const grey = Color(0xff525A6C);
 const whiteTrasparent = Color.fromRGBO(244, 244, 244, 1.0);
--- lib/src/views/screens/homeScreen.dart
+++ lib/src/views/screens/homeScreen.dart
@@ -23,12 +23,13 @@ class HomeScreen extends StatelessWidget {
                     child: ListView.builder(
                       itemBuilder: (context, index) {
                         return Container(
-                          height: 48,
-                          width: 48,
+                          height: 60,
+                          width: 60,
                           margin: const EdgeInsets.all(5.0),
-                          decoration: const BoxDecoration(
-                            color: slateBlue,
+                          decoration: BoxDecoration(
+                            color: whiteColor,
                             shape: BoxShape.circle,
+                            border: Border.all(color: slateBlue, width: 2),
                           ),
                         );
                       },
@@ -37,9 +38,34 @@ class HomeScreen extends StatelessWidget {
                       shrinkWrap: true,
                     ),
                   ),
+                  const VerticalDivider(
+                    width: 0,
+                    thickness: 1,
+                    color: grey,
+                  ),
                   Expanded(
                     flex: 8,
-                    child: Placeholder(),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: size.height / 2,
+                          decoration: const BoxDecoration(color: red),
+                          child: Expanded(
+                              child: Column(
+                            children: [
+                              Row(
+                                children: [Text("data")],
+                              )
+                            ],
+                          )),
+                        ),
+                        const Divider(),
+                        Container(
+                          height: size.height / 2,
+                          decoration: const BoxDecoration(color: blackColor),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
@@ -51,14 +77,68 @@ class HomeScreen extends StatelessWidget {
               child: Container(
                 height: size.height / 9,
                 decoration: BoxDecoration(
-                  color: Colors.red,
+                  color: whiteColor,
                   boxShadow: [
                     BoxShadow(
-                      color: Colors.black.withOpacity(.5),
+                      color: Colors.black.withOpacity(.2),
                       blurRadius: 1,
-                      spreadRadius: 2,
+                      spreadRadius: 1,
                     ),
                   ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 10.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 3,
+                        child: Center(
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.menu,
+                              color: blackColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 9,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Text(
+                                "Kids see a Ghost",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: blackColor,
+                                ),
+                              ),
+                              Text(
+                                "Kaby west and kitui",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.normal,
+                                  color: grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      FloatingActionButton(
+                        backgroundColor: blackColor,
+                        onPressed: () {},
+                        child: const Icon(
+                          Icons.pause,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
